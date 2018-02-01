87861d8fbbc3076c834144f3a3647cf4ab748a1f
antiline <antiline@jun2.org>
2018-02-01 14:16:15 +0900

    Add react options

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: 'airbnb',
+  'globals': {
+    'React': true,
+    'ReactDOM': true,
+  },
+  'plugins': ['react'],
   rules: {
+    'react/jsx-filename-extension': [ON, { 'extensions': ['.jsx'] }],
   },
 };
