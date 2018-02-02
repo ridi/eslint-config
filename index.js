1a1ad2f6b836bd67fd59ac142a2afaf3e964ccf3
kyungmi.k <kyungmi.k@ridi.com>
2018-02-02 11:20:51 +0900

    `jsx-a11y/label-has-for` 규칙 변경
    - 디폴트 설정은 `"required": { "every": ["nesting", "id"] }`로 `<label>` 태그는 항상 컨트롤을 포함하고 각각 `id`와 `htmlFor` 속성을 가져야 함
    - `<label>` 태그가 컨트롤을 포함하는 것이 강제될 경우 RUI의 마크업 방식에 어긋남
    - 따라서 `id`와 `htmlFor` 속성만 가지면 되도록 규칙을 완화

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    // JSX 파일의 확장자를 `.jsx`로 제한
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx'] }],
+
+    // `<label>` 태그가 컨트롤을 반드시 포함하지 않아도 됨
+    // 참고: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+    'jsx-a11y/label-has-for': ['error', { required: 'id' }],
   },
 };
