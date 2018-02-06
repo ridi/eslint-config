a694757a088360f2e7eecb2c6ce3d2296e2f51ef
Koong Kyungmi <purecolor85@gmail.com>
2018-02-06 12:16:48 +0900

    `max-len` 규칙 변경: `140` => `120`

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ module.exports = {
     // 클래스 메서드 안에서 반드시 this를 사용할 필요 없음
     'class-methods-use-this': 'off',
 
-    // 행의 최대 길이는 140
-    'max-len': ['warn', 140],
+    // 행의 최대 길이는 120
+    'max-len': ['warn', 120],
 
     // 문장의 마지막에 존재하는 주석 시작 전에 한 개 이상의 공백을 허용
     'no-multi-spaces': ['error', { ignoreEOLComments: true }],
