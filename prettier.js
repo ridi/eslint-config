6d836e7788b93782c83ec7e51dc925ce4acbc86c
Heejae Jang <j132134@gmail.com>
2020-02-03 11:31:21 +0900

    Update rules for eslint-plugin-prettier

--- prettier.js
+++ prettier.js
@@ -6,6 +6,11 @@ module.exports = {
     'prettier/babel',
     'prettier/react',
   ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: [
     'prettier',
   ],
@@ -13,6 +18,35 @@ module.exports = {
     'prettier/prettier': ['error', {
       trailingComma: 'all',
       singleQuote: true,
+      useTabs: false,
+    }],
+    'arrow-body-style': ['error', 'as-needed'],
+    'prefer-arrow-callback': 'error',
+    curly: ['error', 'all'],
+    'lines-around-comment': ['error', {
+      beforeBlockComment: true,
+      afterBlockComment: true,
+      beforeLineComment: true,
+      afterLineComment: true,
+      allowBlockStart: true,
+      allowBlockEnd: true,
+      allowObjectStart: true,
+      allowObjectEnd: true,
+      allowArrayStart: true,
+      allowArrayEnd: true,
+    }],
+    'max-len': ['error', {
+      code: 80,
+      ignoreUrls: true,
+    }],
+    'no-confusing-arrow': ['error', {
+      allowParens: false,
+    }],
+    'no-mixed-operators': 'error',
+    'no-tabs': 'error',
+    quotes: ['error', 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: false,
     }],
   },
 };
