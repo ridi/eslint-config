0792802b6f72584d9a721db4bce246b61799c93b
Seong Min Park <32555977+notoriousmango@users.noreply.github.com>
2020-12-30 09:27:56 +0900

    패키지 업데이트 및 타입스크립트룰 airbnb로 바꾸기 (#26)
    * feature: fix lint
    
    * feature typescript rules
    
    * remove tsconfig.json
    
    * use eslint-config-typescript
    
    * remove .ds-store from gitignore

--- typescript.js
+++ typescript.js
@@ -1,14 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
-    project: './tsconfig.json', // required for plugin:@typescript-eslint/recommended-requiring-type-checking
+    project: ['./tsconfig.json'], // Specify it only for TypeScript files
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-typescript',
   ],
 };
